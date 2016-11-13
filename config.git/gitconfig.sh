a53ae9971aa1843d1e8be2f0a6ba352c350203eb
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2016-11-13 17:16:13 +0700

    Update Git Config

--- config.git/gitconfig.sh
+++ config.git/gitconfig.sh
@@ -1,15 +1,5 @@
-user.email=fx.budi.ariyanto@gmail.com
-user.name=budiariyanto
-core.repositoryformatversion=0
-core.filemode=true
-core.bare=false
-core.logallrefupdates=true
-remote.origin.url=git@github.com:budiariyanto/personal-config.git
-remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
-branch.master.remote=origin
-branch.master.merge=refs/heads/master
-
+echo "Setting up git config..."
 git config --global user.email "fx.budi.ariyanto@gmail.com"
-git config --global user.name "budiariyanto"
+git config --global user.name "Budi Ariyanto"
 git config --global core.editor "vim"
 git config --global core.autocrlf input
