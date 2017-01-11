d780de873b752f362b122fc8a714da0c4a26e32a
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2017-01-11 22:46:10 +0700

    Change java version to 1.8 on config.sh

--- config.system/config.sh
+++ config.system/config.sh
@@ -1,5 +1,5 @@
 #environtment variables
-export ENV_HOME_JAVA="/data/development/jdk1.7.0_45"
+export ENV_HOME_JAVA="/data/development/jdk1.8.0_112"
 export ENV_HOME_MAVEN="/data/development/apache-maven-3.1.1"
 export ENV_HOME_ANDROID="/data/development/adt-bundle-linux-x86_64-20131030/sdk"
 export ENV_HOME_GRADLE="/data/development/gradle-1.12"
