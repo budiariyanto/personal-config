79a1765381482e95f9b0c382962f9f35423c2313
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2017-02-26 16:45:11 +0700

    Change git config

--- config.git/gitconfig.sh
+++ config.git/gitconfig.sh
@@ -1,5 +1,6 @@
 echo "Setting up git config..."
 git config --global user.email "fx.budi.ariyanto@gmail.com"
 git config --global user.name "Budi Ariyanto"
+git config --global user.signingkey BF53F8D4
 git config --global core.editor "vim"
 git config --global core.autocrlf input
